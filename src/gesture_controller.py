f1021e127db94099f78ce90af4917c49b738ea41
Akanksha Maurya <mauryaakanksha2020@gmail.com>
2025-03-19 21:39:30 +0530

    gesture controller

--- src/gesture_controller.py
+++ src/gesture_controller.py
@@ -465,3 +465,79 @@ class Controller:
                 Controller.pinch_control_init(hand_result)
                 Controller.pinchmajorflag = True
             Controller.pinch_control(hand_result,Controller.changesystembrightness, Controller.changesystemvolume)
+    '''
+----------------------------------------  Main Class  ----------------------------------------
+    Entry point of Gesture Controller
+'''
+
+
+class GestureController:
+    """
+    Handles camera, obtain landmarks from mediapipe, entry point
+    for whole program.
+
+    Attributes
+    ----------
+    gc_mode : int
+        indicates weather gesture controller is running or not,
+        1 if running, otherwise 0.
+    cap : Object
+        object obtained from cv2, for capturing video frame.
+    CAM_HEIGHT : int
+        highet in pixels of obtained frame from camera.
+    CAM_WIDTH : int
+        width in pixels of obtained frame from camera.
+    hr_major : Object of 'HandRecog'
+        object representing major hand.
+    hr_minor : Object of 'HandRecog'
+        object representing minor hand.
+    dom_hand : bool
+        True if right hand is domaniant hand, otherwise False.
+        default True.
+    """
+    gc_mode = 0
+    cap = None
+    CAM_HEIGHT = None
+    CAM_WIDTH = None
+    hr_major = None # Right Hand by default
+    hr_minor = None # Left hand by default
+    dom_hand = True
+
+    def __init__(self):
+        """Initilaizes attributes."""
+        GestureController.gc_mode = 1
+        GestureController.cap = cv2.VideoCapture(0)
+        GestureController.CAM_HEIGHT = GestureController.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        GestureController.CAM_WIDTH = GestureController.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    
+    def classify_hands(results):
+        """
+        sets 'hr_major', 'hr_minor' based on classification(left, right) of 
+        hand obtained from mediapipe, uses 'dom_hand' to decide major and
+        minor hand.
+        """
+        left , right = None,None
+        try:
+            handedness_dict = MessageToDict(results.multi_handedness[0])
+            if handedness_dict['classification'][0]['label'] == 'Right':
+                right = results.multi_hand_landmarks[0]
+            else :
+                left = results.multi_hand_landmarks[0]
+        except:
+            pass
+
+        try:
+            handedness_dict = MessageToDict(results.multi_handedness[1])
+            if handedness_dict['classification'][0]['label'] == 'Right':
+                right = results.multi_hand_landmarks[1]
+            else :
+                left = results.multi_hand_landmarks[1]
+        except:
+            pass
+        
+        if GestureController.dom_hand == True:
+            GestureController.hr_major = right
+            GestureController.hr_minor = left
+        else :
+            GestureController.hr_major = left
+            GestureController.hr_minor = right
